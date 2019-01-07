53e37cc91cfbf1f797e7f40f1ef1835fd405c9b4
Alexander Mykolaichuk <alexmix3000@gmail.com>
2019-01-07 17:19:06 +0200

    added 226. Invert Binary Tree

---
+++ golang/leetcode/226. Invert Binary Tree.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	root.Left, root.Right = root.Right, root.Left
+
+	return root
+}
